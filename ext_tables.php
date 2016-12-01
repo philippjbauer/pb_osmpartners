dcfa8a06aa4de1b96ed42dbf49bcd3089f155ada
Philipp Bauer <hello@philippbauer.org>
2016-12-01 14:59:46 -0700

    Add FlexForm includion into ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,6 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function($extKey)
     {
-
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'PhilippBauer.PbOsmpartners',
             'Pi1',
@@ -19,6 +18,14 @@ call_user_func(
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pbosmpartners_domain_model_partner', 'EXT:pb_osmpartners/Resources/Private/Language/locallang_csh_tx_pbosmpartners_domain_model_partner.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pbosmpartners_domain_model_partner');
 
+        // Add flexform to plugin
+        $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+        $pluginName = strtolower('pi1');
+        $pluginSignature = $extensionName.'_'.$pluginName;
+
+        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+        $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$extKey . '/Configuration/FlexForms/flexform_pi1.xml');
     },
     $_EXTKEY
 );
