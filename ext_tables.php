add323fdd4a752e309aa4d8caaf00c1a0f7cf2fa
Philipp Bauer <hello@philippbauer.org>
2016-12-01 15:38:03 -0700

    Make the ext_tables.php TYPO3 6.2.* compatible

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function($extKey)
-    {
+// call_user_func(
+//     function($extKey)
+//     {
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'PhilippBauer.PbOsmpartners',
             'Pi1',
             'OpenStreetMap Partners'
         );
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'OpenStreetMap Partners');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'OpenStreetMap Partners');
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pbosmpartners_domain_model_map', 'EXT:pb_osmpartners/Resources/Private/Language/locallang_csh_tx_pbosmpartners_domain_model_map.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pbosmpartners_domain_model_map');
@@ -19,13 +19,13 @@ call_user_func(
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pbosmpartners_domain_model_partner');
 
         // Add flexform to plugin
-        $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+        $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
         $pluginName = strtolower('pi1');
         $pluginSignature = $extensionName.'_'.$pluginName;
 
         $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
         $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$extKey . '/Configuration/FlexForms/flexform_pi1.xml');
-    },
-    $_EXTKEY
-);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+//     },
+//     $_EXTKEY
+// );
